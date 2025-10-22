0ffe65ca6b8cc282b2b34a6949b5877f4ed31eb7
VEERA <veera@gmail.com>
2025-10-22 08:25:51 +0530

    v2.1

--- practicejenkinsfullstack/src/config.js
+++ practicejenkinsfullstack/src/config.js
@@ -1,6 +1,8 @@
 // src/config.js
 const config = {
-  url: "http://localhost:2025/api/instagramuser"
+  url: "http://localhost:2000/api/instagramuser"
+  //2000 for local 
+  //2025 for container -host
 };
 
 export default config;
